7db094770e1861e65dcede1d9a568c0f6da67713
yqs <yqsbigmountain@163.com>
2019-12-12 15:54:06 +0800

    用户头像上传预览

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,5 +1,5 @@
-// 当表单发生提交时间
-$('#userForm').on('submit',function(){
+// 当表单发生提交submit事件
+$('#userForm').on('submit', function () {
     var formData = $(this).serialize();
     // 向服务器端发送添加用户的请求
     $.ajax({
@@ -11,10 +11,30 @@ $('#userForm').on('submit',function(){
             location.reload()
         },
         // 添加用户请求失败
-        error:function(){
+        error: function (response) {
             alert('用户添加失败')
         }
     });
     // 阻止表单默认提交
     return false
+})
+
+// 当用户头像发生onchange事件
+$('#avatar').on('change', function () {
+    var formData = new FormData();
+    formData.append('avatar', this.files[0])
+    $.ajax({
+        type: "post",
+        url: "/upload",
+        data: formData,
+        // 告诉ajax不要解析请求参数
+        processData: false,
+        // 告诉ajax不要设置请求参数类型
+        contentType: false,
+        success: function (response) {
+            // 实现头像预览功能
+            $('#preview').attr('src',response[0].avatar);
+            $('#hiddenAvatar').val(response[0].avatar);
+        }
+    });
 })
