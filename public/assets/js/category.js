e09f25416c2f08685a6120080c9ef163962322a6
yqs <yqsbigmountain@163.com>
2019-12-13 10:57:37 +0800

    渲染用户列表

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -1,5 +1,5 @@
 // 当添加分类表单发生提交行为的时候
-$('#addCategory').on('submit',function(){
+$('#addCategory').on('submit', function () {
     // 获取用户在表单中输入的内容
     var formData = $(this).serialize();
     // 向服务器端发送请求 添加分类
@@ -14,3 +14,15 @@ $('#addCategory').on('submit',function(){
     // 阻止表单默认提交
     return false;
 })
+
+// 发送ajax请求,向服务器发送所有用户列表的请求
+$.ajax({
+    type: "get",
+    url: "/categories",
+    success: function (response) {
+        var html = template('categoryListTpl', {
+            data: response
+        });
+        $('#categoryBox').html(html);
+    }
+});
